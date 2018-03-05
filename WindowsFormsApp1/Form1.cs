88a645cd830c5c46f20b38fbc7e0a5d041ab444c
Ming Lung <danny83313@gmail.com>
2018-03-05 15:56:18 +0800

    push file

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -24,6 +24,7 @@ namespace WindowsFormsApp1
             Gst.Application.Init();
             Gst.Element pipeline;
             //string command = "videotestsrc pattern=ball ! queue ! x264enc ! rtph264pay ! queue ! decodebin ! autovideosink";
+            //string command = "rtspsrc location = rtsp://140.130.20.168:8554/RTSP0001 latency=0 ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! decodebin ! autovideosink";
             string command = "rtspsrc location = rtsp://140.130.20.168:8555/RTSP0002 latency=0 ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! decodebin ! autovideosink"; 
             pipeline = Gst.Parse.Launch(command);
             pipeline.SetState(Gst.State.Playing);
